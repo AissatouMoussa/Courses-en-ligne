b7295a8fd39c301d173bc959690b5cfb2b9e8e5c
AissatouMoussa <aichamoussaaissatou@gmail.com>
2022-11-24 23:46:23 +0100

    Création store 1

---
+++ src/app/lib/store.js
@@ -0,0 +1,3 @@
+import { createStore} from "redux"
+import onelineStoreApp from "./reducers"
+export const store = createStore(onelineStoreApp)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './app/views/App';
+import { store } from './app/lib/store'
+import { addtoCart } from './app/lib/actions';
+
+console.log(store.getState())
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+store.dispatch(addtoCart(2, {name: "citron"}))
+store.dispatch(addtoCart(5, {name: "kiwi"}))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
